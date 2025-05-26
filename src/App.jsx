9f2f3574a86aa5d1b320a4b53c5b911fab1e80ba
Chloe Haynes <chloehaynes5252@gmail.com>
2025-05-26 13:44:06 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import Day from './Day.jsx'
+
+function App() {
+  return (
+      <Day />
+  );
+}
+
+export default App
---
+++ src/Day.jsx
@@ -0,0 +1,8 @@
+function Day(){
+    return(
+        <div className="day">
+            <h2 style={{color: 'black'}}>Day</h2>
+        </div>
+    );
+}
+export default Day
